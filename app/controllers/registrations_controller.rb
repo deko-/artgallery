47c89c702fa8399b6203c4cf025e3b09bbb1e526
Dennis Kotsch <dennis@manaflask.com>
2011-05-25 15:55:24 +0200

    fixed typo in registrations_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-class URegistrationsController < Devise::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
   prepend_view_path "app/views/devise"
   def new
     super
