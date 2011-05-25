d575a6bea83d2acc0b3c0e81e43b117834dd2b0c
Joakim Runeberg <joakim.runeberg@gmail.com>
2011-05-25 18:01:15 +0300

    railsadmin improvements and fixing

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,4 +1,14 @@
 RailsAdmin.config do |config|
   config.excluded_models << UserRole
   RailsAdmin.authorize_with :cancan
+  config.excluded_models << Role
+  config.model Artwork do
+    edit do
+      field :name
+      field :description, :text
+      field :created_at
+      field :width
+      field :height
+    end
+  end
 end
